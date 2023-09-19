51c4185eadd40821d76222f2d08ed2dbfd7e1b09
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2023-09-19 17:40:19 +0200

    latest

--- main.jl
+++ main.jl
@@ -13,30 +13,40 @@ end
 end
 
 const rings = Distributed.RemoteChannel(() -> Channel{Any}(10));
-const jobs = Distributed.RemoteChannel(() -> Channel{MatElem}(10));
-const results = Distributed.RemoteChannel(() -> Channel{Tuple}(32));
+const jobs = Distributed.RemoteChannel(() -> Channel{FieldElem}(10));
+const results = Distributed.RemoteChannel(() -> Channel{FieldElem}(32));
 
 @everywhere function do_work(rings, jobs, results) # define work function everywhere
     Qx = take!(rings)
     F = take!(rings)
-    MR = take!(rings)
+
+    println("taking m")
+    m = take!(jobs)
     
+    println(F == parent(m))
+
     for i in 1:10
-        m = take!(jobs)
-        put!(results, (det(m), myid()))
+        println(Qx, F)
+    
+        put!(results, m^2)
     end
 end
+
 
 function make_jobs(n)
     Qx, x = QQ["x"]
     F, a = number_field(x^2 + x + 1)
-    MR = matrix_space(F, 2, 2)
     put!(rings, Qx)
     put!(rings, F)
-    put!(rings, MR)
-    
+
+    println("main")
+    println(Oscar.global_serializer_state.id_to_obj)
     for i in 1:n
-        put!(jobs, MR(Matrix{elem_type(F)}([a^i F(0); a^(i - 2) F(1)])))
+        m = a^i
+        put!(jobs, m)
+        println(Oscar.global_serializer_state.obj_to_id[parent(m)])
+        println(Oscar.global_serializer_state.obj_to_id[F])
+        println(F == parent(m))
     end
 end;
 
@@ -48,10 +58,11 @@ for p in workers() # start tasks on the workers to process requests in parallel
     remote_do(do_work, p, rings, jobs, results)
 end
 
-global total = identity_matrix(QQ, 2)
-
-@elapsed while n > 0 # print out results
-    determinant, worker = take!(results)
-    global n = n - 1
-    total *= determinant
-end
+#global total = identity_matrix(QQ, 2)
+#
+#@elapsed while n > 0 # print out results
+#    determinant, worker = take!(results)
+#    global n = n - 1
+#    total *= determinant
+#end
+#
